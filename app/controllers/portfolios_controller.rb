f2863290ccba5266d3229803a51da9e26b98f1e7
Michael Constantinou <michaelconstantinou@Michaels-MacBook-Pro.local>
2017-12-05 14:01:23 +0000

    added destroy functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,6 +40,17 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    #perform look up
+    @portfolio_item = Portfolio.find(params[:id])
+    #destroy record
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Post was removed.' }
+  end
+
+  end
+
 
 
 
