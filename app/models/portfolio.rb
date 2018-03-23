767b05d8c8657e35c82decf3ea9123a056265255
Michael Constantinou <michaelconstantinou330@gmail.com>
2018-03-23 21:40:52 +0000

    added concerns for placeholder

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   def self.angular
@@ -6,5 +7,11 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_porfolio_items, -> {where(subtitle: 'RoR')}
-#You would need to set method, routes and views accordingly for scope to work.
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: '600', width:'400')
+    self.thumb_image ||= Placeholder.image_generator(height: '250', width:'250')
+  end
 end
