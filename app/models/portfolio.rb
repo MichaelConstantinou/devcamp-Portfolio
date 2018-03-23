b4587c66046255088a56013425c529df5f17dbf6
Michael Constantinou <michaelconstantinou330@gmail.com>
2018-03-23 12:02:40 +0000

    added scope

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
-  validates_prescence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body, :main_image, :thumb_image
+
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+
+  scope :ruby_on_rails_porfolio_items, -> {where(subtitle: 'RoR')}
+#You would need to set method, routes and views accordingly for scope to work.
 end
