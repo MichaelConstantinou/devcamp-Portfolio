e8cf57b7cb53be43bdf477d30c0b43484db4b12a
Michael Constantinou <michaelconstantinou330@gmail.com>
2018-05-10 12:08:21 +0100

    added params for editing and registering account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :set_source
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def set_source
-    session[:source] = params[:q] if params[:q]
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  # before_action :set_source
+
+  # def set_source
+  #   session[:source] = params[:q] if params[:q]
+  # end
 end
